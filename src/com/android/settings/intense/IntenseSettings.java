f5ac6e50df83a80512257a5f93b8281e50e75cb6
vibhu0009 <cvibhu0009@gmail.com>
2015-12-19 15:34:26 +0100

    Intense tweaks bringup
    Conflicts:
    	res/values/intense_strings.xml

---
+++ src/com/android/settings/intense/IntenseSettings.java
@@ -0,0 +1,38 @@
+package com.android.settings.intense;
+
+import android.os.Bundle;
+import android.content.res.Resources;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+import android.provider.Settings;
+
+import com.android.internal.logging.MetricsLogger;
+
+public class IntenseSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "IntenseSettings";
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.APPLICATION;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.intense_settings);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        return false;
+    }
+}
